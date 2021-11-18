ecc0c4516ac3219a4956cb84081f2ca0d82fb5cc
Deacon Sowerby <61608431+Deacon8@users.noreply.github.com>
2021-11-18 15:04:05 -0800

    Makefiles

--- GameEngine/build.bat
+++ GameEngine/build.bat
@@ -1,5 +1,2 @@
 @echo off
-#remove -g and -wall for non-debug build
-gcc -g -Wall src/Test.c -o bin/Test.exe
-cd bin
-Test.exe
+mingw32-make
--- GameEngine/src/Test.c
+++ GameEngine/src/Test.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include "stuff.h"
 
 int main()
 {
 	printf("Hello World!");
 	int i;
+	printf("%i\n", TestInclude);
 	scanf("%i", &i);
 	return(0);
 }
